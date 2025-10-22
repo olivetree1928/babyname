fd7e0aba6624d567a6af777fa793e3ba3ca44990
olivetree1928 <olivetree1928@gmail.com>
2025-10-22 15:41:55 +0800

    vercel problem

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,18 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
   server: {
     host: '0.0.0.0', // 允许从任何IP访问
     port: 5173,
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    minify: 'terser',
+    chunkSizeWarningLimit: 1600,
   },
 });
